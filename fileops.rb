4d0dcb624dbd58a6def80e5249974dca3e7a0191
James Abbott <james@jamesabbottdd.com>
2012-10-22 15:45:43 +0200

    tidying up

--- fileops.rb
+++ fileops.rb
@@ -2,14 +2,19 @@ require 'minitest/spec'
 require 'minitest/autorun'
 
 def parse_1(file_path)
-  arr = IO.read(file_path).split("\n")
-  return arr unless arr.include? ""
-  arr.select! {|line| !line.empty?}
+  IO.read(file_path).split("\n").select! {|line| !line.empty?}
 end
 
 def parse_2(file_path)
   IO.read(file_path).split("\n")
 end
+
+def parse_3(file_path)
+  arr = IO.read(file_path).split("\n")
+  return arr unless arr.include? ""
+  arr.select! {|line| !line.empty?}
+end
+
 =begin
 here-doc of the desired string, compare it to various inputs (with / without leading
 empty lines) and methods, and find the method that parses all possible inputs correctly.
@@ -32,7 +37,9 @@ describe "parses the input to a standard form regardless of the number of blank 
   end
   it "parses uniformly" do
     parse_1('files/example.txt').must_equal @arr
-    parse_1('files/example-1.txt').must_equal @arr
+    parse_2('files/example-1.txt').must_equal @arr
+    parse_3('files/example.txt').must_equal @arr
+    parse_3('files/example-1.txt').must_equal @arr
   end
 end
 

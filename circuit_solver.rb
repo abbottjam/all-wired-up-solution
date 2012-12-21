283ac9074c4ac026ff13def99f34a052f4cdb88f
James Abbott <james@jamesabbottdd.com>
2012-12-21 20:24:07 +0100

    final cleanup

--- circuit_solver.rb
+++ circuit_solver.rb
@@ -1,9 +1,6 @@
 require_relative "lib/evaluator.rb"
 require_relative "lib/parser.rb"
 
-#require File.expand_path('lib/evaluator.rb', File.dirname(__FILE__))
-#require File.expand_path('lib/parser.rb', File.dirname(__FILE__))
-
 def resolve(path)
   expressions = Circuit.new(path).parse_all
   values = expressions.map {|val| eval(transform(val))}

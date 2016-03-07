c69b4eadac35412420d39ca6f3ccc7954e0c91bb
James Abbott <james@jamesabbottdd.com>
2016-03-07 11:19:57 +0100

    improved grouping of test code, fixed indentation issues

--- lib/parser.rb
+++ lib/parser.rb
@@ -9,150 +9,150 @@ class Circuit
     @tree = IO.read(file_path).split("\n")
   end
 
-# -> string
-def parse
-  parse_tree(move_left(find_root))
-end
-
-# -> array of expressions
-def parse_all
-  find_roots.map do |coor|
-    parse_tree(move_left(coor))
+  # () -> string
+  def parse
+    parse_tree(move_left(find_root))
   end
-end
 
-# -> position
-def find_root
-  tree.each_with_index do |str, i|
-    if str.match '@'
-      y = str.index '@'
-      return {:row => i, :col => y}
+  # () -> array of expressions
+  def parse_all
+    find_roots.map do |coor|
+      parse_tree(move_left(coor))
     end
   end
-end
 
-# -> array of positions
-def find_roots
-  roots = []
-  tree.each_with_index do |str, i|
-    if str.match '@'
-      y = str.index '@'
-      roots << {:row => i, :col => y}
+  # () -> position
+  def find_root
+    tree.each_with_index do |str, i|
+      if str.match '@'
+        y = str.index '@'
+        return {:row => i, :col => y}
+      end
     end
   end
-  roots
-end
 
-#position -> string
-def parse_tree(pos)
-  token = get(pos)
-  return token if is_operand? token
-  left = parse_left_subtree(pos)
-  right = parse_right_subtree(pos)
-  if token.eql? 'N'
-    ['(', token, left, right, ')'].join #preorder
-  else
-    ['(', left, token, right, ')'].join #inorder
+  # () -> array of positions
+  def find_roots
+    roots = []
+    tree.each_with_index do |str, i|
+      if str.match '@'
+        y = str.index '@'
+        roots << {:row => i, :col => y}
+      end
+    end
+    roots
   end
-end
 
-#position -> procedure or string
-def parse_left_subtree(pos)
-  if left_subtree? pos
-    parse_tree(move_left(move_up(pos)))
-  else
-    ''
+  # (position) -> string
+  def parse_tree(pos)
+    token = get(pos)
+    return token if is_operand? token
+    left = parse_left_subtree(pos)
+    right = parse_right_subtree(pos)
+    if token.eql? 'N'
+      ['(', token, left, right, ')'].join # preorder traversal
+    else
+      ['(', left, token, right, ')'].join # inorder traversal
+    end
   end
-end
 
-#position -> procedure or string
-def parse_right_subtree(pos)
-  if right_subtree? pos
-    parse_tree(move_left(move_down(pos)))
-  else
-    ''
+  # (position) -> procedure | string
+  def parse_left_subtree(pos)
+    if left_subtree? pos
+      parse_tree(move_left(move_up(pos)))
+    else
+      ''
+    end
   end
-end
 
-#position -> boolean
-def left_subtree?(pos)
-  get(up(pos)).eql? '|'
-end
-
-#position -> boolean
-def right_subtree?(pos)
-  get(down(pos)).eql? '|'
-end
-
-#position -> token or nil
-def get(pos)
-  r = pos[:row]
-  c = pos[:col]
-  begin
-    tree[r][c]
-  rescue Exception => e
-    puts e.message
+  # (position) -> procedure | string
+  def parse_right_subtree(pos)
+    if right_subtree? pos
+      parse_tree(move_left(move_down(pos)))
+    else
+      ''
+    end
   end
-end
 
-#position -> position or procedure
-def move_left(pos)
-  token = get(pos)
-  return pos if is_symbol? token
-  move_left(left(pos))
-end
+  # (position) -> boolean
+  def left_subtree?(pos)
+    get(up(pos)).eql? '|'
+  end
 
-#position -> position or procedure
-def move_up(pos)
-  return pos if turn? pos
-  move_up(up(pos))
-end
+  # (position) -> boolean
+  def right_subtree?(pos)
+    get(down(pos)).eql? '|'
+  end
 
-#position -> position or procedure
-def move_down(pos)
-  return pos if turn? pos
-  move_down(down(pos))
-end
+  # (position) -> token | nil
+  def get(pos)
+    r = pos[:row]
+    c = pos[:col]
+    begin
+      tree[r][c]
+    rescue Exception => e
+      puts e.message
+    end
+  end
 
-#position -> boolean
-def turn?(pos)
-  get(pos).eql?('|') &&
-  get(left(pos)).eql?('-')
-end
+  # (position) -> position | procedure
+  def move_left(pos)
+    token = get(pos)
+    return pos if is_symbol? token
+    move_left(left(pos))
+  end
 
-#position -> position
-def left(pos)
-  {
-    :row => pos[:row],
-    :col => pos[:col]-1
-  }
-end
+  # (position) -> position | procedure
+  def move_up(pos)
+    return pos if turn? pos
+    move_up(up(pos))
+  end
 
-#position -> position
-def up(pos)
-  {
-    :row => pos[:row]-1,
-    :col => pos[:col]
-  }
-end
+  # (position) -> position | procedure
+  def move_down(pos)
+    return pos if turn? pos
+    move_down(down(pos))
+  end
 
-#position -> position
-def down(pos)
-  {
-    :row => pos[:row]+1,
-    :col => pos[:col]
-  }
-end
+  # (position) -> boolean
+  def turn?(pos)
+    get(pos).eql?('|') &&
+    get(left(pos)).eql?('-')
+  end
 
-def is_operator?(token)
-  OPERATORS.include? token
-end
+  # (position) -> position
+  def left(pos)
+    {
+      :row => pos[:row],
+      :col => pos[:col]-1
+    }
+  end
 
-def is_operand?(token)
-  OPERANDS.include? token
-end
+  # (position) -> position
+  def up(pos)
+    {
+      :row => pos[:row]-1,
+      :col => pos[:col]
+    }
+  end
 
-def is_symbol?(token)
-  is_operator?(token) || is_operand?(token)
-end
+  # (position) -> position
+  def down(pos)
+    {
+      :row => pos[:row]+1,
+      :col => pos[:col]
+    }
+  end
+
+  def is_operator?(token)
+    OPERATORS.include? token
+  end
+
+  def is_operand?(token)
+    OPERANDS.include? token
+  end
+
+  def is_symbol?(token)
+    is_operator?(token) || is_operand?(token)
+  end
 end
--- test/tests.rb
+++ test/tests.rb
@@ -5,7 +5,8 @@ require_relative "../circuit_solver.rb"
 require 'minitest/spec'
 require 'minitest/autorun'
 
-describe "evaluator tests" do
+# EVALUATOR
+describe "Evaluator tests" do
   describe "transforming input sequence" do
     it "substitutes symbols with values" do
       transform('((0A1)X(N1))').must_equal '((false&&true)^(!true))'
@@ -25,94 +26,99 @@ describe "evaluator tests" do
   end
 end
 
-describe "parser tests" do
+# PARSER
+describe "Parser tests" do
   describe "parsing the symbol tree" do
-   before do
-    @c = Circuit.new('./files/input/example.txt')
-  end
+    before do
+      @c = Circuit.new('./files/input/example.txt')
+    end
 
-  describe "finding the root" do
-    it "returns the coordinates" do
-      @c.find_root.must_equal({:row => 3, :col => 40})
+    describe "finding the root" do
+      it "returns the coordinates" do
+        @c.find_root.must_equal({:row => 3, :col => 40})
+      end
     end
-  end
 
-  describe "moving left" do
-    it "returns the coordinates of the first operator encountered" do
-      root_coor = {:row => 3, :col => 40}
-      @c.move_left(root_coor).must_equal({:row => 3, :col => 27})
+    describe "moving left" do
+      it "returns the coordinates of the first operator encountered" do
+        root_coor = {:row => 3, :col => 40}
+        @c.move_left(root_coor).must_equal({:row => 3, :col => 27})
+      end
     end
-  end
 
-  describe "parsing an operand - base case" do
-    it "returns the operand" do
-      @c.parse_tree({:row => 0, :col => 0}).must_equal '0'
-      @c.parse_tree({:row => 2, :col => 0}).must_equal '1'
+    describe "parsing an operand - base case" do
+      it "returns the operand" do
+        @c.parse_tree({:row => 0, :col => 0}).must_equal '0'
+        @c.parse_tree({:row => 2, :col => 0}).must_equal '1'
+      end
     end
-  end
 
-  describe "parsing an operator - recursive case" do
-    it "returns the stringified expression" do
-      @c.parse_tree({:row => 1, :col => 14}).must_equal '(0A1)'
+    describe "parsing an operator - recursive case" do
+      it "returns the stringified expression" do
+        @c.parse_tree({:row => 1, :col => 14}).must_equal '(0A1)'
+      end
     end
-  end
 
-  describe "when meeting a turn: -|" do
-    it "recognizes a turn" do
-      @c.turn?({:row => 0, :col => 14}).must_equal true
-      @c.turn?({:row => 2, :col => 14}).must_equal true
+    describe "when meeting a turn: -|" do
+      it "recognizes a turn" do
+        @c.turn?({:row => 0, :col => 14}).must_equal true
+        @c.turn?({:row => 2, :col => 14}).must_equal true
+      end
     end
-  end
 
-  describe "addressing individual tokens with get(pos)" do
-    it "returns the token or nil" do
-      pos = {:row => 5, :col => 14}
-      @c.get(pos).must_equal 'N'
-      above = @c.up(pos)
-      @c.get(above).must_equal '|'
-      below = @c.down(pos)
-      @c.get(below).must_equal nil
+    describe "addressing individual tokens with get(pos)" do
+      it "returns the token or nil" do
+        pos = {:row => 5, :col => 14}
+        @c.get(pos).must_equal 'N'
+        above = @c.up(pos)
+        @c.get(above).must_equal '|'
+        below = @c.down(pos)
+        @c.get(below).must_equal nil
+      end
     end
-  end
 
-  describe "meeting a subtree" do
-    it "recognizes subtree" do
-      @c.left_subtree?({:row => 5, :col => 14}).must_equal true
+    describe "meeting a subtree" do
+      it "recognizes subtree" do
+        @c.left_subtree?({:row => 5, :col => 14}).must_equal true
+      end
+      it "recogizes missing subtree - edge case" do
+        @c.right_subtree?({:row => 5, :col => 14}).must_equal false
+      end
     end
-    it "recogizes missing subtree - edge case" do
-      @c.right_subtree?({:row => 5, :col => 14}).must_equal false
-    end
-  end
 
-  describe "parsing the whole tree" do
-    it "returns the stringified expression" do
-      @c.parse.must_equal '((0A1)X(N1))'
+    describe "parsing the whole tree" do
+      it "returns the stringified expression" do
+        @c.parse.must_equal '((0A1)X(N1))'
+      end
     end
-  end
 
-  describe "parsing simple circuits" do
-    it "parses single circuits to correct parenthesized expressions" do
-      Circuit.new('./files/input/simple-1.txt').parse.must_equal '(0O1)'
-      Circuit.new('./files/input/simple-2.txt').parse.must_equal '((0A1)X(N1))'
-      Circuit.new('./files/input/simple-3.txt').parse.must_equal '((0O1)X(1X1))'
-    end
-    it "parses several circuits to correct parenthesized expressions" do
-      Circuit.new('./files/input/simple_circuits.txt').parse_all.must_equal ['(0O1)', '((0A1)X(N1))', '((0O1)X(1X1))']
+    describe "parsing simple circuits" do
+      it "parses single circuits to correct parenthesized expressions" do
+        Circuit.new('./files/input/simple-1.txt').parse.must_equal '(0O1)'
+        Circuit.new('./files/input/simple-2.txt').parse.must_equal '((0A1)X(N1))'
+        Circuit.new('./files/input/simple-3.txt').parse.must_equal '((0O1)X(1X1))'
+      end
+      it "parses several circuits to correct parenthesized expressions" do
+        Circuit.new('./files/input/simple_circuits.txt').parse_all.must_equal ['(0O1)', '((0A1)X(N1))', '((0O1)X(1X1))']
+      end
     end
   end
 end
 
-describe "driver tests" do
+# DRIVER
+describe "Driver tests" do
   describe "simple circuits evaluate to correct output" do
-  it "works" do
-    resolve('./files/input/simple_circuits.txt').must_equal ['on', 'off', 'on']
+    it "works" do
+      resolve('./files/input/simple_circuits.txt').must_equal ['on', 'off', 'on']
+    end
+  end
+
+  describe "complex circuits evaluate to correct output" do
+    it "works, too" do
+      resolve('./files/input/complex_circuits.txt').must_equal ["on", "on", "on", "off", "off", "on", "on", "off"]
+    end
   end
 end
 
-describe "complex circuits evaluate to correct output" do
-  it "works, too" do
-    resolve('./files/input/complex_circuits.txt').must_equal ["on", "on", "on", "off", "off", "on", "on", "off"]
-  end
-end
-end
-end
+
+
